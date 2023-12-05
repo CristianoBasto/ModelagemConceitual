95dd8b4dfe0a034a8eebeff434667a200ac0dcbd
Cristiano bastos <cristhianobastos@gmail.com>
2023-12-04 23:54:15 -0300

    Criando operação de estnciação

--- src/main/java/com/nelioalves/cursomc/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/nelioalves/cursomc/modelagemconceitual/ModelagemconceitualApplication.java
@@ -1,13 +1,33 @@
 package com.nelioalves.cursomc.modelagemconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemconceitualApplication {
+import com.nelioalves.cursomc.modelagemconceitual.domain.Categoria;
+import com.nelioalves.cursomc.modelagemconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemconceitualApplication implements CommandLineRunner{
+    
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informticas");
+		Categoria cat2 = new Categoria(null, "Escritorios");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
