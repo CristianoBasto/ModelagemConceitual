8189d89d9bd9ac9beee3928b142c18016766bd1a
Cristiano bastos <cristhianobastos@gmail.com>
2023-12-24 16:36:51 -0300

    estados e Cidades

--- src/main/java/com/nelioalves/cursomc/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/nelioalves/cursomc/modelagemconceitual/ModelagemconceitualApplication.java
@@ -1,6 +1,7 @@
 package com.nelioalves.cursomc.modelagemconceitual;
 
 
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.modelagemconceitual.domain.Categoria;
+import com.nelioalves.cursomc.modelagemconceitual.domain.Cidade;
+import com.nelioalves.cursomc.modelagemconceitual.domain.Estado;
 import com.nelioalves.cursomc.modelagemconceitual.domain.Produto;
 import com.nelioalves.cursomc.modelagemconceitual.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.modelagemconceitual.repositories.CidadeRepository;
+import com.nelioalves.cursomc.modelagemconceitual.repositories.EstadoRepository;
 import com.nelioalves.cursomc.modelagemconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +26,12 @@ public class ModelagemconceitualApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtorepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
@@ -43,9 +54,24 @@ public class ModelagemconceitualApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));	
 		
-		
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 		produtorepository.saveAll(Arrays.asList(p1,p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlandia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
+	
+		
 	}
 
 }
--- src/main/java/com/nelioalves/cursomc/modelagemconceitual/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/modelagemconceitual/resources/CategoriaResource.java
@@ -3,9 +3,9 @@ package com.nelioalves.cursomc.modelagemconceitual.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.modelagemconceitual.domain.Categoria;
@@ -18,10 +18,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService  service;
 	
-    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity<?> find(@PathVariable Integer id) {
-    	
-    	
+
     	Categoria obj = service.buscar(id);
     	return ResponseEntity.ok().body(obj);
     	
